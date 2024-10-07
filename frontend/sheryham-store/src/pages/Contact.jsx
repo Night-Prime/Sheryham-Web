10da8b2edeeeb292d28c146ca75fa7e5a958a837
Daniel Abati <danielabatibabatunde1@gmail.com>
2024-10-07 17:06:23 +0100

    a push

--- frontend/sheryham-store/src/pages/Contact.jsx
+++ frontend/sheryham-store/src/pages/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
     <section className="h-screen w-screen">
       <main className="sw-full h-3/4 flex flex-col justify-center items-center text-center gap-6">
         <h1 className="font-bold text-4xl md:text-6xl">
-          SHERYHAMTEES MULTI-CHOICE LTD
+          SHERYHAMTEES MULTI-CHOICE LTD.
         </h1>
         <p className="font-semibold text-xl">Info@sheryham.com</p>
         <p className="text-md">
